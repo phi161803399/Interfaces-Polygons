cc438219e05a1f57ec3e7f67c4699db2c2b5f79a
Mark Huntjens <mark_huntjens@reyrey.com>
2018-11-22 08:57:14 +0100

    FirstCommit

---
+++ Polygons.Library/ConcreteRegularPolygon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Polygons.Library
+{
+    public class ConcreteRegularPolygon
+    {
+        //public int NumberOfSides { get; set; }
+        private int _numberOfSides;
+
+        public int NumberOfSides
+        {
+            get { return _numberOfSides; }
+            set { _numberOfSides = value; }
+        }
+
+        public int SideLength { get; set; }
+
+        public ConcreteRegularPolygon(int sides, int length)
+        {
+            NumberOfSides = sides;
+            SideLength = length;
+        }
+
+        public double GetPerimeter()
+        {
+            return NumberOfSides * SideLength;
+        }
+
+        public virtual double GetArea()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
